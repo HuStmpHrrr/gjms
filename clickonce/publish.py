ddb8858912ae519c3a2bf9ec970695913433b831
HuStmpHrrr <HuStmpHrrr@users.noreply.github.com>
2015-06-05 13:09:42 -0400

    Update publish.py

--- clickonce/publish.py
+++ clickonce/publish.py
@@ -20,7 +20,13 @@ target_shares = {
     'dev'    : []
     }
 # it needs this transformation because msbuild does a direct string concatenation instead of a path join.
-target_shares = {k: [p if p.endswith(os.path.sep) else p+os.path.sep for p in v] for k, v in target_shares.items()}
+def unify_path(p):
+    if isinstance(p, str):
+        p = p if p.endswith(os.path.sep) else p+os.path.sep
+        return (p, p)
+    else:
+        return p
+target_shares = {k: [unify_path(p) for p in v] for k, v in target_shares.items()}
 
 output_dir = r'bin\publish'
 publish_dir = r'bin\publishapp.publish'
@@ -30,6 +36,10 @@ msbuild_folder = r'%ProgramFiles%\MSBuild\12.0\bin\amd64' \
 
 
 def get_appver():
+    if not os.path.exists(appver_file):
+        with open(appver_file) as fd:
+            fd.write('1.0.0.0')
+            return '1.0.0.0'
     with open(appver_file) as fd:
         return fd.readline().strip()
 
@@ -42,24 +52,25 @@ def set_appver(ver):
     with open(appver_file, 'w') as fd:
         fd.write(ver)
 
-def get_cmd(target, ver, env):
-    template = r'"{0}\msbuild" /t:clean;publish /property:OutputPath={1},PublishUrl={2},InstallUrl={2},UpdateUrl={2},ApplicationVersion={3},MinimumRequiredVersion={3},AssemblyName="{4}"'
-    cmd = template.format(msbuild_folder, output_dir, target, ver, 'NST System Configurator '+env)
+def get_cmd(target, updateurl, ver, env):
+    template = r'"{0}\msbuild" /t:clean;publish /property:OutputPath={1},PublishUrl={2},InstallUrl={2},UpdateUrl={3},ApplicationVersion={4},MinimumRequiredVersion={4},AssemblyName="{5}"'
+    cmd = template.format(msbuild_folder, output_dir, target, updateurl, ver, 'NST System Configurator '+env)
     return cmd
 
 if __name__=='__main__':
     error = {}
     print('current python implementation version is', sys.version)
-    print('currently working in: %s' % pwd)
+    print('currently working in: {}'.format(pwd))
     print('please make sure this script runs directly under the project folder.')
-    env = input('build environment(%s): ' % ', '.join(sorted(target_shares.keys())))
+    env = input('build environment({}): '.format(', '.join(sorted(target_shares.keys()))))
     while env not in target_shares:
         print("nonexisting environment: {}".format(env), file=sys.stderr)
-        env = input('build environment(%s): ' % ', '.join(sorted(target_shares.keys())))
+        env = input('build environment({}): '.format(', '.join(sorted(target_shares.keys()))))
 
     ver = incr_appver(get_appver())
     for i, p in enumerate(target_shares[env]):
-        cmd = get_cmd(p, ver, env+str(i))
+        target, updateutl = p
+        cmd = get_cmd(target, updateurl, ver, env+str(i))
 
         print('executing {}'.format(cmd))
         print('----------------------------------')
@@ -72,25 +83,25 @@ if __name__=='__main__':
 
         if proc.returncode == 0:
             try:
-                distutils.dir_util.copy_tree(publish_dir, p)                
+                distutils.dir_util.copy_tree(publish_dir, target)                
             except Exception as e:
-                error[p] = e
-                sys.stderr.write("error occurred: %s\n" % str(e))
+                error[target] = e
+                print('error occurred: {}'.format(str(e)), file=sys.stderr)
                 distutils.dir_util.copy_tree(publish_dir, r'bin\backup' + '\\' + str(i))
         else:
-            print("error: %d" % proc.returncode, file=sys.stderr)
+            print("error: {}".format(proc.returncode), file=sys.stderr)
         print
 
     if len(error) != 0:
         print('Error occurred:', file=sys.stderr)
         for k, e in error.items():
-            print('%s: %s\n' % (k, str(e)), file=sys.stderr)
+            print('{}: {}'.format(k, str(e)), file=sys.stderr)
         print('has backed up the folder.', file=sys.stderr)
 
     try:
         set_appver(ver)
     except IOError as e:
-        print("failed to write to file: %s" % str(e), file=sys.stderr)
-        print('next application version will be %s.' % incr_appver(ver), file=sys.stderr)
+        print("failed to write to file: {}".format(str(e)), file=sys.stderr)
+        print('next application version will be {}.'.format(incr_appver(ver)), file=sys.stderr)
 
     input('press enter to continue...')
